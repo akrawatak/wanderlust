3fbe479eced668453f144095da0681d720c793c0
akash Rawat <akrawatak25@gmail.com>
2024-02-24 20:56:26 +0530

    added

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,7 @@
 const Listing = require("../models/listing.js");
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapToken = process.env.MAP_TOKEN;
-const geocodingClient = mbxGeocoding({ acessToken: mapToken });
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index =  async (req, res) => {
     const allListings = await Listing.find({});
@@ -34,13 +34,14 @@ module.exports.showListing = (async (req, res) => {
 module.exports.createListing =// (  
     async (req, res, next) => {
 
-       let response = await geocodingClient.forwardGeocode({
+       let response = await geocodingClient
+       .forwardGeocode({
             query: req.body.listing.location,
             limit: 1,
           })
             .send();
-          console.log(response.body.features[0].geometry);
-          res.send("done");
+        //  console.log(response.body.features[0].geometry);
+        
 
 
         let url =req.file.path;
@@ -48,7 +49,11 @@ module.exports.createListing =// (
         const newListing = new Listing(req.body.listing);
         newListing.owner = req.user._id;
         newListing.image = {url,filename};
-        await newListing.save();
+
+        newListing.geometry = response.body.features[0].geometry;
+
+     let savedListing =   await newListing.save();
+     console.log(savedListing);
         req.flash("success", "New Listing Created!");
         res.redirect("/listings");
     };
--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,21 @@
 
-mapboxgl.accessToken = mapToken;
- //'pk.eyJ1IjoiYWthc2g5ODczIiwiYSI6ImNsc3d1ZnVqNjExdmYyaXF0eGE5dXhyZjcifQ.9FjUIqXPZ4oD55-tz9Rtpw';
- console.log(mapToken);
+	mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
-center: [77.2090,28.6139], // starting position [lng, lat]
+center:listing.geometry.coordinates, // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
+
+
+
+
+const marker = new mapboxgl.Marker({color:'red'})
+.setLngLat(listing.geometry.coordinates)
+.setPopup(new mapboxgl.Popup({offset: 25})
+.setHTML(`<h4>${listing.location}</h4><p>Exact Location will be provided after booking</p>`)
+
+)
+.addTo(map);
+
+
+
